ea0f4907ed59c0e11868e6ac1427ed5378c5cc21
RAFFAELESCI <scisciotta.raffaele@gmail.com>
2025-08-01 13:58:17 +0000

    test commit

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// firebase-config.js
+const firebaseConfig = {
+  ,
+apiKey: "AIzaSyDtSDqUKdsmID1MrN07BKoo6ZFe6ymisSg"  authDomain: "il-club-della-vasca.firebaseapp.com",
+  projectId: "il-club-della-vasca",
+  storageBucket: "il-club-della-vasca.appspot.com",
+  messagingSenderId: "924753798835",
+  appId: "1:924753798835:web:fc21f386c666caf37f32c6",
+  measurementId: "G-EZHK6WKYWW"
+};
+
